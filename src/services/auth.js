4eac15ef03e21fc57982ab82519387b1e8d0b6d2
Kevin Cavalcanti <k.w9cavalcanti@gmail.com>
2019-05-28 11:05:39 -0300

    Configuração básica da variável de autenticação

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,1 +1,6 @@
 export const TOKEN_KEY = "";
+
+/**
+ * O parâmetro isAuthenticated verificará o localStorage
+ * se existe um token JWT retornado do servidor backend */
+export const isAuthenticated = () => true;
