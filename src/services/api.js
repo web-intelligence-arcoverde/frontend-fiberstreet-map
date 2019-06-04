11ac5921ea69779bb0f85fd730d15264d8d2b79c
Kevin Cavalcanti <k.w9cavalcanti@gmail.com>
2019-06-04 10:27:20 -0300

    Added services api and auth

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,21 @@
 import axios from "axios";
+import { getToken } from "./api";
+
+const serverOne = "http://localhost:3333";
+const serverTwo = "http://localhost:3001";
 
 /** This API will connect to NodeJS Server */
 const api = axios.create({
-  baseURL: "http://localhost:3333"
+  baseURL: serverTwo//"http://localhost:3333"
 });
+
+// api.interceptors.request.use(async config => {
+//   const token = getToken();
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return config;
+// });
 
 export default api;
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,14 @@
-export const TOKEN_KEY = "";
+export const TOKEN_KEY = "@fiberstreet-token";
 
 /**
  * O parâmetro isAuthenticated verificará o localStorage
  * se existe um token JWT retornado do servidor backend */
-export const isAuthenticated = () => true;
+export const isAuthenticated = () => true; //true;
+export const isAuthenticatedServer = () =>
+  localStorage.getItem(TOKEN_KEY) !== null;
+
+export const login = token => {
+  localStorage.setItem(TOKEN_KEY, token);
+};
+export const logout = () => localStorage.removeItem(TOKEN_KEY);
+export const getToken = () => localStorage.getItem(TOKEN_KEY);
