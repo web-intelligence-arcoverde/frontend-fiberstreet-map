50954a2ad97a971352df6ec3c0f0574a55728f61
˜Kevin <k.w9cavalcanti@icloud.com>
2019-07-08 08:54:06 -0300

    combineReducers dos ducks DuckPattern

---
+++ src/redux/store/ducks/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+
+import all from "./all";
+import ctos from "./ctos";
+// import { reducer } from "../../store";
+
+export default combineReducers({
+  all,
+  ctos
+});
