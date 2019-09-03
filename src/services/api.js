57aa8df2d4e89313d2bf60d9dcedd4ae934b04f2
Kevin Cavalcanti <k.w9cavalcanti@gmail.com>
2019-09-03 16:52:35 +0000

    Update/lucas

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,17 @@
 import axios from "axios";
 import { getToken } from "./api";
 
-const serverOne = "http://localhost:3334";
-const serverTwo = "http://localhost:3001";
+// const serverOne = "http://localhost:3334";
+// const serverTwo = "http://localhost:3001";
+const server = "http://192.168.0.131:3334";
+const local = 'http://45.224.40.252:3339';
 
 /** This API will connect to NodeJS Server */
 const api = axios.create({
-  baseURL: serverOne //"http://localhost:3333"
+  baseURL: server
 });
+
+
 
 // api.interceptors.request.use(async config => {
 //   const token = getToken();
