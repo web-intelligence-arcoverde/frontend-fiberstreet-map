6b90720749ce1732d6a893e86d0c67d716f1bca7
Lucas Henrique Paes de Carvalho <lukas.paes18@gmail.com>
2019-09-19 12:54:02 +0000

    Organização dos componentes

--- src/pages/MapO/index.js
+++ src/pages/MapO/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import mapboxgl from "mapbox-gl";
+import lineDistance from "turf-line-distance";
 import {
   ScatterplotLayer,
   GeoJsonLayer,
@@ -17,7 +18,6 @@ import api, { API } from "../../services/api";
 import * as Actions from "../../redux/store/actions/all";
 import { Creators as CtosActions } from "../../redux/store/ducks/ctos";
 import { Creators as MapCreators } from "../../redux/store/ducks/map";
-
 import Button from "./button";
 
 const myDeckLayer = new MapboxLayer({
@@ -96,6 +96,119 @@ class Map extends Component {
       zoom: zoom
     });
 
+    var distanceContainer = document.getElementById("distance");
+
+    // GeoJSON object to hold our measurement features
+    var geojson = {
+      type: "FeatureCollection",
+      features: []
+    };
+
+    // Used to draw a line between points
+    var linestring = {
+      type: "Feature",
+      geometry: {
+        type: "LineString",
+        coordinates: []
+      }
+    };
+
+    map.on("load", function() {
+      map.addSource("geojson", {
+        type: "geojson",
+        data: geojson
+      });
+
+      // Add styles to the map
+      map.addLayer({
+        id: "measure-points",
+        type: "circle",
+        source: "geojson",
+        paint: {
+          "circle-radius": 5,
+          "circle-color": "#000"
+        },
+        filter: ["in", "$type", "Point"]
+      });
+      map.addLayer({
+        id: "measure-lines",
+        type: "line",
+        source: "geojson",
+        layout: {
+          "line-cap": "round",
+          "line-join": "round"
+        },
+        paint: {
+          "line-color": "#000",
+          "line-width": 2.5
+        },
+        filter: ["in", "$type", "LineString"]
+      });
+
+      map.on("click", function(e) {
+        var features = map.queryRenderedFeatures(e.point, {
+          layers: ["measure-points"]
+        });
+
+        // Remove the linestring from the group
+        // So we can redraw it based on the points collection
+        if (geojson.features.length > 1) geojson.features.pop();
+
+        // Clear the Distance container to populate it with a new value
+        distanceContainer.innerHTML = "";
+
+        // If a feature was clicked, remove it from the map
+        if (features.length) {
+          var id = features[0].properties.id;
+          geojson.features = geojson.features.filter(function(point) {
+            return point.properties.id !== id;
+          });
+        } else {
+          var point = {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [e.lngLat.lng, e.lngLat.lat]
+            },
+            properties: {
+              id: String(new Date().getTime())
+            }
+          };
+
+          geojson.features.push(point);
+        }
+
+        if (geojson.features.length > 1) {
+          linestring.geometry.coordinates = geojson.features.map(function(
+            point
+          ) {
+            return point.geometry.coordinates;
+          });
+
+          geojson.features.push(linestring);
+
+          // Populate the distanceContainer with total distance
+
+          var value = document.createElement("pre");
+          value.textContent =
+            "Total distance: " +
+            lineDistance(linestring, "kilometers").toLocaleString() +
+            "km";
+          distanceContainer.appendChild(value);
+        }
+
+        map.getSource("geojson").setData(geojson);
+      });
+    });
+
+    map.on("mousemove", function(e) {
+      var features = map.queryRenderedFeatures(e.point, {
+        layers: ["measure-points"]
+      });
+      // UI indicator for clicking/hovering a point on the map
+      map.getCanvas().style.cursor = features.length ? "pointer" : "crosshair";
+    });
+
     // this.setState({map})
     this.state.map = map;
     // Configura eventos de clique no mapa
@@ -107,6 +220,9 @@ class Map extends Component {
   firstConfigure() {
     let { map } = this.state;
     const { showDataInViewModal, addMapSuccess } = this.props;
+    //Regua
+
+    //Fim regua
 
     map.on("click", e => this.handleMapClick(e));
     var url = "https://wanderdrone.appspot.com";
@@ -475,9 +591,12 @@ class Map extends Component {
     const { polyline } = this.props.redux.all.mapa;
 
     return (
-      <div id={container} className={classNameStyle}>
-        <Header />
-      </div>
+      <>
+        <div id="distance" class="distance-container"></div>
+        <div id={container} className={classNameStyle}>
+          <Header />
+        </div>
+      </>
     );
   }
 }
