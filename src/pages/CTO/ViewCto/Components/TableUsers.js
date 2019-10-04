66b5f58aed708db22d387bd71fdbc37c3f7ba1f2
Luca Henrique <lukas.paes18@gmail.com>
2019-10-04 09:54:56 -0300

    atualização nos componets da viewCTO

--- src/pages/CTO/ViewCto/Components/TableUsers.js
+++ src/pages/CTO/ViewCto/Components/TableUsers.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { lighten, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import DeleteIcon from '@material-ui/icons/Delete';
-import FilterListIcon from '@material-ui/icons/FilterList';
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import { lighten, makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import TableSortLabel from "@material-ui/core/TableSortLabel";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
+import DeleteIcon from "@material-ui/icons/Delete";
+import FilterListIcon from "@material-ui/icons/FilterList";
 
 function createData(cpf, nome, plano, pppoe, port) {
-  return { cpf, nome, plano, pppoe,port};
+  return { cpf, nome, plano, pppoe, port };
 }
 
 const rows = [
-  createData('1', 'Lucas', 30, '@ppoe1', 1),
-  createData('2', 'Henrique', 250, '@ppoe2',2),
-  createData('3', 'Paes', 160, '@ppoe3',3),
-  createData('4', 'Carvalho', 60, '@ppoe4',4),
-  createData('5', 'João', 160, '@ppoe5',5),
+  createData("1", "Lucas", 30, "@ppoe1", 1),
+  createData("2", "Henrique", 250, "@ppoe2", 2),
+  createData("3", "Paes", 160, "@ppoe3", 3),
+  createData("4", "Carvalho", 60, "@ppoe4", 4),
+  createData("5", "João", 160, "@ppoe5", 5)
 ];
 
 function desc(a, b, orderBy) {
@@ -51,40 +51,50 @@ function stableSort(array, cmp) {
 }
 
 function getSorting(order, orderBy) {
-  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+  return order === "desc"
+    ? (a, b) => desc(a, b, orderBy)
+    : (a, b) => -desc(a, b, orderBy);
 }
 
 const headCells = [
-  { id: 'cpf', numeric: false, disablePadding: true, label: 'CPF' },
-  { id: 'nome', numeric: true, disablePadding: false, label: 'Nome' },
-  { id: 'plano', numeric: true, disablePadding: false, label: 'Plano' },
-  { id: 'pppoe', numeric: true, disablePadding: false, label: 'PPPOE' },
-  { id: 'port', numeric: true, disablePadding: false, label: 'Porta' },
+  { id: "cpf", numeric: false, disablePadding: true, label: "CPF" },
+  { id: "nome", numeric: true, disablePadding: false, label: "Nome" },
+  { id: "plano", numeric: true, disablePadding: false, label: "Plano" },
+  { id: "pppoe", numeric: true, disablePadding: false, label: "PPPOE" },
+  { id: "port", numeric: true, disablePadding: false, label: "Porta" }
 ];
 
 function EnhancedTableHead(props) {
-  const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+  const {
+    classes,
+    onSelectAllClick,
+    order,
+    orderBy,
+    numSelected,
+    rowCount,
+    onRequestSort
+  } = props;
   const createSortHandler = property => event => {
     onRequestSort(event, property);
   };
 
   return (
-    <TableHead >
-      <TableRow>  
+    <TableHead>
+      <TableRow>
         <TableCell padding="checkbox">
           <Checkbox
-            style={{color:'#FFBF00'}}
+            style={{ color: "#FFBF00" }}
             indeterminate={numSelected > 0 && numSelected < rowCount}
             checked={numSelected === rowCount}
             onChange={onSelectAllClick}
-            inputProps={{ 'aria-label': 'select all desserts' }}
+            inputProps={{ "aria-label": "select all desserts" }}
           />
         </TableCell>
         {headCells.map(headCell => (
           <TableCell
             key={headCell.id}
-            align={headCell.numeric ? 'right' : 'left'}
-            padding={headCell.disablePadding ? 'none' : 'default'}
+            align={headCell.numeric ? "right" : "left"}
+            padding={headCell.disablePadding ? "none" : "default"}
             sortDirection={orderBy === headCell.id ? order : false}
           >
             <TableSortLabel
@@ -95,7 +105,7 @@ function EnhancedTableHead(props) {
               {headCell.label}
               {orderBy === headCell.id ? (
                 <span className={classes.visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
                 </span>
               ) : null}
             </TableSortLabel>
@@ -111,36 +121,36 @@ EnhancedTableHead.propTypes = {
   numSelected: PropTypes.number.isRequired,
   onRequestSort: PropTypes.func.isRequired,
   onSelectAllClick: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
   orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
+  rowCount: PropTypes.number.isRequired
 };
 
 const useToolbarStyles = makeStyles(theme => ({
   root2: {
     paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(1),
+    paddingRight: theme.spacing(1)
   },
   highlight:
-    theme.palette.type === 'light'
+    theme.palette.type === "light"
       ? {
           color: theme.palette.secondary.main,
-          backgroundColor: lighten('#FFBF00', 0.5),
+          backgroundColor: lighten("#FFBF00", 0.5)
         }
       : {
-          color:'#D8D8D8',
-          backgroundColor: theme.palette.secondary.dark,
+          color: "#D8D8D8",
+          backgroundColor: theme.palette.secondary.dark
         },
   spacer: {
-    flex: '1 1 100%',
+    flex: "1 1 100%"
   },
   actions: {
-    color: theme.palette.text.secondary,
+    color: theme.palette.text.secondary
   },
   title: {
-    flex: '0 0 auto',
-    color: '#FFF',
-  },
+    flex: "0 0 auto",
+    color: "#FFF"
+  }
 }));
 
 const EnhancedTableToolbar = props => {
@@ -150,25 +160,22 @@ const EnhancedTableToolbar = props => {
   return (
     <Toolbar
       className={clsx(classes.root, {
-        [classes.highlight]: numSelected > 0,
+        [classes.highlight]: numSelected > 0
       })}
-      
     >
-      <div className={classes.title} >
+      <div className={classes.title}>
         {numSelected > 0 ? (
           <Typography variant="subtitle1">
             {numSelected} selecionado(s)
           </Typography>
         ) : (
-          <Typography variant="h6" id="tableTitle" style={{color:'#FFBF00', paddingLeft:'160px'}}>
-            Clientes
-          </Typography>
+          <Typography></Typography>
         )}
       </div>
       <div className={classes.spacer} />
-      <div className={classes.actions} >
+      <div className={classes.actions}>
         {numSelected > 0 ? (
-          <Tooltip title="Delete">
+          <Tooltip title="Excluir">
             <IconButton aria-label="delete">
               <DeleteIcon />
             </IconButton>
@@ -186,49 +193,48 @@ const EnhancedTableToolbar = props => {
 };
 
 EnhancedTableToolbar.propTypes = {
-  numSelected: PropTypes.number.isRequired,
+  numSelected: PropTypes.number.isRequired
 };
 
 const useStyles = makeStyles(theme => ({
   root: {
-    width: '100%',
-    marginTop: theme.spacing(3),
+    width: "100%",
+    marginTop: theme.spacing(3)
   },
   paper: {
-    width: '498px',
-    marginBottom: '0px', 
+    width: "100%",
+    marginBottom: "0px"
   },
   table: {
-    minWidth: 750,
+    minWidth: "100%"
   },
   tableWrapper: {
-    overflowX: 'auto',
+    overflowX: "auto"
   },
   visuallyHidden: {
     border: 0,
-    clip: 'rect(0 0 0 0)',
+    clip: "rect(0 0 0 0)",
     height: 1,
     margin: -1,
-    overflow: 'hidden',
+    overflow: "hidden",
     padding: 0,
-    position: 'absolute',
+    position: "absolute",
     top: 20,
-    width: 1,
-  },
+    width: 1
+  }
 }));
 
 export default function EnhancedTable() {
   const classes = useStyles();
-  const [order, setOrder] = React.useState('asc');
-  const [orderBy, setOrderBy] = React.useState('calories');
+  const [order, setOrder] = React.useState("asc");
+  const [orderBy, setOrderBy] = React.useState("calories");
   const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
-
   function handleRequestSort(event, property) {
-    const isDesc = orderBy === property && order === 'desc';
-    setOrder(isDesc ? 'asc' : 'desc');
+    const isDesc = orderBy === property && order === "desc";
+    setOrder(isDesc ? "asc" : "desc");
     setOrderBy(property);
   }
 
@@ -254,7 +260,7 @@ export default function EnhancedTable() {
     } else if (selectedIndex > 0) {
       newSelected = newSelected.concat(
         selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1),
+        selected.slice(selectedIndex + 1)
       );
     }
 
@@ -270,21 +276,23 @@ export default function EnhancedTable() {
     setPage(0);
   }
 
-
   const isSelected = name => selected.indexOf(name) !== -1;
 
-  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
 
   return (
-    <div className={classes.root2} >
-      <Paper className={classes.paper} >
-        <EnhancedTableToolbar  style={{marginTop:'0px'}} numSelected={selected.length} />
-        <div className={classes.tableWrapper} >
+    <div className={classes.root2}>
+      <Paper className={classes.paper}>
+        <EnhancedTableToolbar
+          style={{ marginTop: "0px" }}
+          numSelected={selected.length}
+        />
+        <div className={classes.tableWrapper}>
           <Table
             className={classes.table}
             aria-labelledby="tableTitle"
-            size={'small'}
-            
+            size={"medium"}
           >
             <EnhancedTableHead
               classes={classes}
@@ -294,7 +302,6 @@ export default function EnhancedTable() {
               onSelectAllClick={handleSelectAllClick}
               onRequestSort={handleRequestSort}
               rowCount={rows.length}
-              
             />
             <TableBody>
               {stableSort(rows, getSorting(order, orderBy))
@@ -316,18 +323,32 @@ export default function EnhancedTable() {
                       <TableCell padding="checkbox">
                         <Checkbox
                           checked={isItemSelected}
-                          inputProps={{ 'aria-labelledby': labelId }}
-                          style={{color:'#FFBF00'}}
+                          inputProps={{ "aria-labelledby": labelId }}
+                          style={{ color: "#FFBF00" }}
                         />
                       </TableCell>
-                      
-                      <TableCell component="th" style={{color:'#BDBDBD'}} id={labelId} scope="row" padding="none">
+
+                      <TableCell
+                        component="th"
+                        style={{ color: "#BDBDBD" }}
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
                         {row.cpf}
                       </TableCell>
-                      <TableCell align="right" style={{color:'#BDBDBD'}}>{row.nome}</TableCell>
-                      <TableCell align="right" style={{color:'#BDBDBD'}}>{row.plano}</TableCell>
-                      <TableCell align="right" style={{color:'#BDBDBD'}}>{row.pppoe}</TableCell>
-                      <TableCell align="right" style={{color:'#BDBDBD'}}>{row.port}</TableCell>
+                      <TableCell align="right" style={{ color: "#BDBDBD" }}>
+                        {row.nome}
+                      </TableCell>
+                      <TableCell align="right" style={{ color: "#BDBDBD" }}>
+                        {row.plano}
+                      </TableCell>
+                      <TableCell align="right" style={{ color: "#BDBDBD" }}>
+                        {row.pppoe}
+                      </TableCell>
+                      <TableCell align="right" style={{ color: "#BDBDBD" }}>
+                        {row.port}
+                      </TableCell>
                     </TableRow>
                   );
                 })}
@@ -346,10 +367,10 @@ export default function EnhancedTable() {
           rowsPerPage={rowsPerPage}
           page={page}
           backIconButtonProps={{
-            'aria-label': 'previous page',
+            "aria-label": "previous page"
           }}
           nextIconButtonProps={{
-            'aria-label': 'next page',
+            "aria-label": "next page"
           }}
           onChangePage={handleChangePage}
           onChangeRowsPerPage={handleChangeRowsPerPage}
